5abddcfdaa91d9c063b06c17bd3a48d8563e73ab
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-07-30 16:57:44 -0700

    Working on the JS portion.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Challenge 1: Your Age in Days
 
-
 function ageInDays() {
     var birthYear = prompt('What year were you born?');
     var ageInDayss = (2021 - birthYear) * 365;
@@ -13,4 +12,12 @@ function ageInDays() {
 
 function reset() {
     document.getElementById('ageInDays').remove();
+}
+
+// Challenge 2: Cat Generator
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    imag.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
 }
